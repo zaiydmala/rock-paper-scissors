e47ea1ae3602e059da59ddfdf2e3571cbb717df2
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-02 10:18:01 +0530

    Declare lose conditions for player

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+
+    if(playerChoice.toLowerCase() === computerChoice.toLowerCase()) { console.log("It is a draw!")}
+
+    else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper" ||
+            playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors" ||
+            playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock" ) {
+                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+            }
+    
+    
+}
+var playerChoice = prompt("Choose Rock, Paper or Scissors!");
+var computerChoice = getComputerChoice();
+console.log(playRound(playerChoice, computerChoice));
+
+
+
