6e54138d7507f948512b61dbafe5063de60644ec
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-02 10:36:45 +0530

    Declare win conditions for player and announce score after the round

--- index.js
+++ index.js
@@ -7,19 +7,33 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
 
-    if(playerChoice.toLowerCase() === computerChoice.toLowerCase()) { console.log("It is a draw!")}
+    var playerScore = 0;
+    var computerScore = 0;
+
+    if(playerChoice.toLowerCase() === computerChoice.toLowerCase()) { console.log("It is a draw!")} //declare draw condition
 
     else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper" ||
             playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors" ||
             playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock" ) {
                 console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
-            }
-    
-    
+                computerScore++;
+                
+            }   //declare lose conditions and outputs
+    else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors" ||
+            playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock" ||
+            playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "paper") {
+                console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
+                playerScore++;
+            
+    }   //declare win conditions and outputs
+    return score = `${playerScore} - ${computerScore}`;
 }
-var playerChoice = prompt("Choose Rock, Paper or Scissors!");
-var computerChoice = getComputerChoice();
-console.log(playRound(playerChoice, computerChoice));
+let playerChoice = prompt("Choose Rock, Paper or Scissors!");
+computerChoice =getComputerChoice();
+playRound(playerChoice, computerChoice);
+console.log(score);
+
+
 
 
 
