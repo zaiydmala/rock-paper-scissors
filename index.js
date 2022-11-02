617518c35a0a0909f84232266a58a387c77f13af
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-03 01:18:38 +0530

    Create game function to run 5rounds

--- index.js
+++ index.js
@@ -1,37 +1,48 @@
-choices = ["rock","paper","scissors"];
-
-function getComputerChoice() {
-    var computerChoice = choices[Math.floor(Math.random()*choices.length)];
-    return computerChoice;
-}
-
-function playRound(playerChoice, computerChoice) {
-
+function game() {
+    var choices = ["rock","paper","scissors"];
     var playerScore = 0;
     var computerScore = 0;
+    
 
-    if(playerChoice.toLowerCase() === computerChoice.toLowerCase()) { console.log("It is a draw!")} //declare draw condition
+    for(i = 0; i < 5; i++) {
 
-    else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper" ||
-            playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors" ||
-            playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock" ) {
-                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
-                computerScore++;
-                
-            }   //declare lose conditions and outputs
-    else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors" ||
-            playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock" ||
-            playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "paper") {
-                console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
-                playerScore++;
-            
-    }   //declare win conditions and outputs
-    return score = `${playerScore} - ${computerScore}`;
+        playRound();
+
+        function playRound(playerChoice, computerChoice) {
+
+            var playerChoice = prompt("Choose Rock, Paper or Scissors!");
+            var computerChoice = getComputerChoice();
+            function getComputerChoice() {
+                var computerChoice = choices[Math.floor(Math.random()*choices.length)];
+                return computerChoice;
+            }
+            if(playerChoice.toLowerCase() === computerChoice.toLowerCase()) { console.log("It is a draw!"); } //declare draw condition
+        
+            else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "paper" ||
+                    playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "scissors" ||
+                    playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "rock" ) {
+                        console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+                        computerScore++;
+                        return computerScore;
+                  
+                    }   //declare lose conditions and outputs
+
+            else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissors" ||
+                    playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock" ||
+                    playerChoice.toLowerCase() === "scissors" && computerChoice.toLowerCase() === "paper") {
+                        console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
+                        playerScore++;
+                        return playerScore;
+                     
+                    }   //declare win conditions and outputs
+            return score = `${playerScore} - ${computerScore}`; 
+        }
+    }
+    console.log(score);
+    
 }
-let playerChoice = prompt("Choose Rock, Paper or Scissors!");
-computerChoice =getComputerChoice();
-playRound(playerChoice, computerChoice);
-console.log(score);
+game();
+
 
 
 
