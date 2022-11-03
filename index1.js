ac6a4aea9afd50dd90ed199adebb18833303d3a9
Zaiyd Mala <52519982+zaiydMala@users.noreply.github.com>
2022-11-04 01:19:57 +0530

    Add draw and losing condition to the game of 5 rounds

--- index1.js
+++ index1.js
@@ -1,9 +1,10 @@
 game();
 function game() {
-    var choices = ["rock", "paper", "scissors"];
+    var choices = ["r", "p", "s"];
     var playerScore = 0;
     var computerScore = 0;
     console.log(`${playerScore} - ${computerScore}`);
+    
     for(i = 0; i < 5; i++) {
         playRound();
         function playRound() {
@@ -12,8 +13,19 @@ function game() {
             function getComputerChoice() {
                 let computerChoice = choices[Math.floor(Math.random()*choices.length)];
                 return computerChoice;
-            }
-            console.log(`${playerChoice} - ${computerChoice}`);
+            }//Get a random choice of the computer and returns it and stores in the variable computerChoice
+            // console.log(`${playerChoice} - ${computerChoice}`);
+
+            if(playerChoice.toLowerCase() === computerChoice.toLowerCase()) { console.log("It is a draw!"); }
+            else if(playerChoice.toLowerCase() === "r" && computerChoice.toLowerCase() === "p" ||
+            playerChoice.toLowerCase() === "p" && computerChoice.toLowerCase() === "s" ||
+            playerChoice.toLowerCase() === "s" && computerChoice.toLowerCase() === "r") {
+                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+                computerScore++;
+            }//declare losing conditions and incrementing the computer's score
+
+
+            console.log(`${playerScore} - ${computerScore}`);
 
         }
 
